8b43677a2da0d782d9f9beec82529445de0dbdad
Henrik Cullen <hccullen@users.noreply.github.com>
2023-03-16 14:13:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const sendAudioFileToApi = async (file, auth) => {
         }, {
             headers: {
                 'Content-Type': 'multipart/form-data',
-                'Authorization': `Bearer ${auth}`
+                'Authorization': auth
             }
         });
         const output = {
